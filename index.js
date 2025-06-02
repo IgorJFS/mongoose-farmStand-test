d9a1949ca4e666b37439547aa4ef76ebd825fc99
IgorJFS <igormpaz@gmail.com>
2025-06-02 08:23:47 -0300

    final stage

--- index.js
+++ index.js
@@ -22,19 +22,28 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const categories = ['fruit', 'vegetable', 'dairy'];
+
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('Products/index', { products });
+    const { category } = req.query;
+    if (category) {
+       const products = await Product.find({ category });
+       res.render('products/index', { products, category });
+    } else {
+       const products = await Product.find({});
+       res.render('products/index', { products, category: 'All' });
+    }
 });
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 });
 
 app.post('/products', async (req, res) => {
@@ -61,7 +70,7 @@ app.get('/products/:id/edit', async (req, res) => {
     if (!product) {
         return res.status(404).send('Product not found');
     }
-    res.render('products/edit', { product });
+    res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async (req, res) => {
@@ -73,6 +82,25 @@ app.put('/products/:id', async (req, res) => {
     console.log(`Product updated: ${updatedProduct}`);
     res.redirect(`/products/${updatedProduct._id}`);
 })
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) {
+        return res.status(404).send('Product not found');
+    }
+    console.log(`Product deleted: ${deletedProduct.name}`);
+    res.redirect('/products');
+});
+
+app.get('/products/:id/delete', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        return res.status(404).send('Product not found');
+    }
+    res.render('products/delete', { product });
+});
 
 
 const PORT = process.env.PORT || 3000;
